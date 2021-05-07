3aec585e4d6e45ca6dd43daaa108f6f4695be5a8
Roberto Arruda <roberto0arruda@hotmail.com>
2021-05-06 22:10:34 -0400

    Create an app using Vue.js

---
+++ start/index.js
@@ -0,0 +1,9 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            productName: 'Book a Cruise to the Moon',
+            productDescription: 'Cruise to the moon in our luxurious shuttle. Watch the astronauts working outside the International Space Station.',
+            // additional properties later
+        }
+    },
+});
